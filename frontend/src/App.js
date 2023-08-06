a24051200b501321881d2bcc29cc1e73b433fc00
Eya-ltaief <eya.letaief1@gmail.com>
2023-08-06 23:02:57 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+function App() {
+  return (
+   <BrowserRouter>
+   <div>
+      <header>
+        <a href='/'>eSHOP</a>
+      </header>
+      <main>
+        <Routes>
+          <Route path='/product/:slug' element={<ProductScreen/>}></Route>
+          <Route path='/' element={<HomeScreen/>}></Route>
+        </Routes>
+      </main>
+   </div>
+   </BrowserRouter>
+  );
+}
+
+export default App;
